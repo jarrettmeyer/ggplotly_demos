7e5699a6db152afdc876ea60fc642b25da887d0b
Jarrett Meyer <meyer_jarrett@lilly.com>
2019-04-24 11:26:17 -0400

    added r_packages to travis

--- deploy.R
+++ deploy.R
@@ -1,12 +1,9 @@
-# Make sure that the rsconnect package is installed.
-if (("rsconnect" %in%) == FALSE) {
-    install.packages("rsconnect")
-}
+library(rsconnect)
 
 # Set the account info for deployment.
-rsconnect::setAccountInfo(name   = Sys.getenv("shinyapps_name"),
-                          token  = Sys.getenv("shinyapps_token"),
-                          secret = Sys.getenv("shinyapps_secret"))
+setAccountInfo(name   = Sys.getenv("shinyapps_name"),
+               token  = Sys.getenv("shinyapps_token"),
+               secret = Sys.getenv("shinyapps_secret"))
 
 # Deploy the application.
-rsconnect::deployApp()
+deployApp()
